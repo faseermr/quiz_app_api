9af27bca8ed1eea1d50b810937c23dea06c2cf8d
unknown <studentreesaf@gmail.com>
2022-10-06 10:37:04 +0530

    updated subject, classroom

--- src/main/java/com/online_exam/server/classroom/ClassroomController.java
+++ src/main/java/com/online_exam/server/classroom/ClassroomController.java
@@ -3,12 +3,10 @@ package com.online_exam.server.classroom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api")
@@ -26,11 +24,26 @@ public class ClassroomController {
     Classroom _classroom = classroomRepositary.save(new Classroom(classroom.getGrade()));
     return _classroom;
    }
-@GetMapping("/classrooms/{id}")
+    @GetMapping("/classrooms/{id}")
    public ResponseEntity<Classroom> getClassroomById(@PathVariable("id") long id) {
         Classroom _classroom = classroomRepositary.findById(id).orElseThrow();
 
         return new ResponseEntity<>(_classroom, HttpStatus.OK);
+
+   }
+   @PutMapping("classrooms/{id}")
+   public ResponseEntity<Classroom> updateTutorial(@PathVariable("id") long id, @RequestBody Classroom classroom){
+       Classroom _classroom = classroomRepositary.findById(id).orElseThrow();
+       _classroom.setGrade(classroom.getGrade());
+
+       return new ResponseEntity<>(classroomRepositary.save(_classroom),HttpStatus.OK);
+   }
+
+
+   @DeleteMapping("classrooms/{id}")
+   public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+        classroomRepositary.findById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
    }
 }
--- src/main/java/com/online_exam/server/subject/SubjectController.java
+++ src/main/java/com/online_exam/server/subject/SubjectController.java
@@ -1,23 +1,25 @@
 package com.online_exam.server.subject;
 
+import com.online_exam.server.classroom.Classroom;
+import com.online_exam.server.classroom.ClassroomRepositary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/api")
 public class SubjectController {
 
     @Autowired
     private SubjectRepositary subjectRepositary;
+
+    @Autowired
+    private ClassroomRepositary classroomRepositary;
 
     @GetMapping("/subjects")
     public ResponseEntity<List<Subject>> getAllSubject() {
@@ -29,5 +31,26 @@ public class SubjectController {
         }catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    @GetMapping("/subjects/{id}")
+    public ResponseEntity<Subject> getAllSubjectById(@PathVariable("id") long id) {
+        Subject _subject = subjectRepositary.findById(id).orElseThrow();
+        return new ResponseEntity<>(_subject,HttpStatus.OK);
+    }
+
+    @PostMapping("/subjects/{clsid}")
+    public ResponseEntity<Subject> addSubject(@PathVariable("clsid") long clsid, @RequestBody Subject subject){
+        Classroom classroom = classroomRepositary.findById(clsid).orElseThrow();
+        subject.setClassroom(classroom);
+        subjectRepositary.save(subject);
+       return new ResponseEntity<>(subject,HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("subjects/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+        subjectRepositary.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 }
